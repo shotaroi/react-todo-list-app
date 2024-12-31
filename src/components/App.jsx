704a1b3c6eb81694b163c42542bddcd7900a5683
shotaroi <58347058+shotaroi@users.noreply.github.com>
2024-12-31 02:34:00 +0100

    Add form and button for todo

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import '../App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Todo-list</h1>
+      <form action="submit">
+        <input type="text" />
+        <button>Add ToDo</button>
+      </form>
+    </>
+  )
+}
+
+export default App
